d97cce3857d37822fe26b21729d255de7fc35e3d
Alexey Kutumov <alexey.kutumov@gmail.com>
2014-09-27 15:50:04 +0700

    Added nestl_printers.hpp for pretty printing some nestl stuff

--- tests/test_common.hpp
+++ tests/test_common.hpp
@@ -1,27 +1,13 @@
 #ifndef NESTL_TESTS_TEST_COMMON_HPP
 #define NESTL_TESTS_TEST_COMMON_HPP
 
-#include <system_error>
-#include <ostream>
-
+#include "tests/nestl_printers.hpp"
 #include <gmock/gmock.h>
 
-#include <nestl/noncopyable.hpp>
-#include <nestl/operation_error.hpp>
-
-inline std::ostream& operator <<(std::ostream& strm, const std::error_condition& ec)
-{
-    strm << "error_condition { message : " << ec.message() << ", value: " << ec.value() << " };";
-    return strm;
-}
 
 
-template <typename Result, typename OperationError>
-inline std::ostream& operator <<(std::ostream& strm, const nestl::result_with_operation_error<Result, OperationError>& ec)
-{
-    strm << "result_with_operation_error { message : " << ec.error().message() << ", value: " << ec.error().value() << " };";
-    return strm;
-}
+
+
 
 
 namespace nestl
