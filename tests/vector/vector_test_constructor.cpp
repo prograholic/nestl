e1f97c592eb3495917e99d9d3041bb47570a2925
Alexey Kutumov <alexey.kutumov@gmail.com>
2017-07-16 13:12:22 +0700

    added implementation of several constructors and `push_back` methods

--- tests/vector/vector_test_constructor.cpp
+++ tests/vector/vector_test_constructor.cpp
@@ -7,12 +7,14 @@ namespace test
 
 NESTL_ADD_TEST(vector_test_constructor)
 {
+    // check default ctor
     {
         nestl::vector<int> vec;
 
         CheckVectorSize(vec, 0);
     }
 
+    // check ctor with allocator
     {
         nestl::allocator<int> a;
         nestl::vector<int> vec(a);
@@ -20,6 +22,7 @@ NESTL_ADD_TEST(vector_test_constructor)
         CheckVectorSize(vec, 0);
     }
 
+    // check move ctor (with empty source)
     {
         nestl::vector<int> vec1;
 
@@ -29,6 +32,7 @@ NESTL_ADD_TEST(vector_test_constructor)
         CheckVectorSize(vec1, 0);
     }
 
+    // check move ctor (with non-empty source)
     {
         nestl::vector<int> vec1;
 
@@ -138,6 +142,99 @@ NESTL_ADD_TEST(vector_test_constructor)
         CheckVectorSize(vec2, 10);
         CheckVectorSize(vec1, 0);
     }
+
+#if NESTL_HAS_EXCEPTIONS == 1
+    ////////////////////////////////////////////////////////////////////////////////
+    /// Now check throwable constructors
+    ////////////////////////////////////////////////////////////////////////////////
+
+    // check copy construction from empty vector
+    {
+        nestl::vector<int> v0;
+        nestl::vector<int> v1{v0};
+
+        CheckVectorSize(v1, 0);
+    }
+
+    // check copy construction from empty noexcept vector
+    {
+        nestl::no_exceptions::vector<int> nxv0;
+        nestl::vector<int> v1{ nxv0 };
+
+        CheckVectorSize(v1, 0);
+    }
+
+    // check copy construction from non-empty vector
+    {
+        nestl::vector<int> v0;
+        v0.push_back(42);
+
+        nestl::vector<int> v1{v0};
+
+        CheckVectorSize(v0, 1);
+        CheckVectorSize(v1, 1);
+
+        NESTL_CHECK_EQ(42, v0[0]);
+        NESTL_CHECK_EQ(42, v1[0]);
+    }
+
+    // check copy construction from non-empty noexcept vector
+    {
+        nestl::no_exceptions::vector<int> nxv0;
+        NESTL_CHECK_OPERATION(nxv0.push_back_nothrow(_, 42));
+
+        nestl::vector<int> v1{ nxv0 };
+
+        CheckVectorSize(nxv0, 1);
+        CheckVectorSize(v1, 1);
+
+        NESTL_CHECK_EQ(42, nxv0[0]);
+        NESTL_CHECK_EQ(42, v1[0]);
+    }
+
+    // check move construction from empty vector
+    {
+        nestl::vector<int> v0;
+        nestl::vector<int> v1{ std::move(v0) };
+
+        CheckVectorSize(v1, 0);
+    }
+
+    // check move construction from empty noexcept vector
+    {
+        nestl::no_exceptions::vector<int> nxv0;
+        nestl::vector<int> v1{ std::move(nxv0) };
+
+        CheckVectorSize(v1, 0);
+    }
+
+    // check move construction from non-empty vector
+    {
+        nestl::vector<int> v0;
+        v0.push_back(42);
+
+        nestl::vector<int> v1{ std::move(v0) };
+
+        CheckVectorSize(v0, 0);
+        CheckVectorSize(v1, 1);
+
+        NESTL_CHECK_EQ(42, v1[0]);
+    }
+
+    // check move construction from non-empty noexcept vector
+    {
+        nestl::no_exceptions::vector<int> nxv0;
+        NESTL_CHECK_OPERATION(nxv0.push_back_nothrow(_, 42));
+
+        nestl::vector<int> v1{ std::move(nxv0) };
+
+        CheckVectorSize(nxv0, 0);
+        CheckVectorSize(v1, 1);
+
+        NESTL_CHECK_EQ(42, v1[0]);
+    }
+
+#endif /* NESTL_HAS_EXCEPTIONS */
 }
 
 } // namespace test
